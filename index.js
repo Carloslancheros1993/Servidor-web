e1a525b675e2255f5d7386e7c1dd86bb712a038f
Carlos <u2204240@unimilitar.edu.co>
2021-05-04 07:17:56 -0500

    simplificando codigo usando switch y funciones de elctura de archivos dependiendo de la cabecera

--- index.js
+++ index.js
@@ -1,37 +1,53 @@
 let http = require("http");
 let fs = require("fs");
+let path = require("path");
 
 http.createServer((req, res) => {
     res.setHeader("Content-Type", "text/html; charset=utf-8");
     switch(req.url){
         case "/contacto":
-            readFile("./contact.html", res);
+            readFile("/contact.html", res);
             break;
         case "/":
-            readFile("./index.html", res);
+            readFile("/index.html", res);
             break;
         case "/nosotros":
-            readFile("./about.html", res);
+            readFile("/about.html", res);
             break;
         case "/proyectos":
-            readFile("./projects.html", res);
+            readFile("/projects.html", res);
             break;
         case "/favicon.ico":
             res.setHeader("Content-Type", "image/x-icon");
-            readFile("./favicon.ico", res);
+            readFile("/favicon.ico", res);
             break;
         default:
-            res.statusCode = 404;
-            readFile("./404.html", res);
+            readFile(req.url, res);
             break;
     }
 }).listen(8080);
                
-const readFile = (path, res) => {
-    fs.readFile(path, (error, content) => {
+const readFile = (url, res) => {
+    let urlF = __dirname + url;
+    //__dirname ruta absoluta
+    fs.readFile( urlF, (error, content) => {
         if(!error){
-            res.write(content);
-            res.end();
+            setContentType(path.extname(urlF), res);
+            //res.setHeader("Content-Type", "text/css");
+            res.end(content);
+        }else{
+            //response.statusCode = 404;
+            res.writeHead(404);
+            res.end("<h1>404</h1>")
         }
-    })
+    });
+}
+
+//cambiar cabecera dependiendo del tipo
+const setContentType = (ext, res) => {
+    if (ext == ".css"){
+        res.setHeader("Content-Type", "text/css");
+    }else if(ext === ".html"){
+        res.setHeader("Content-Type", "text/html");
+    }
 }
